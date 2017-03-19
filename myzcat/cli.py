02e6c5719da4dcecd9c94be6be43cd9fdb386791
Yoav Ram <yoav@yoavram.com>
2017-03-19 12:57:12 +0200

    use logging

--- myzcat/cli.py
+++ myzcat/cli.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import logging
+import warnings
+
 import click
 
 import myzcat
@@ -9,10 +12,15 @@ import myzcat
 	type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
 @click.version_option(version=myzcat.__version__)
 def main(filename):
+	logging.info("{} started with filename {}".format(myzcat.__name__, filename))
 	try:
 		click.echo(myzcat.gzip_reader.read_gzip_file(filename))
 	except OSError as e:
-		raise click.FileError(filename, str(e))
+		err = click.FileError(filename, str(e))
+		logging.error(str(err))
+		raise err
+	else:
+		logging.debug('Reading {} succeeded'.format(filename))
 
 
 if __name__ == '__main__':
